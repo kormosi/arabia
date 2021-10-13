c4a163bf88486460cda48eaf4af37daa9e1357cb
Patrik Kormosi <pkormosi@frinx.io>
2021-10-13 21:11:19 +0200

    Add Arabia borders, detect collision w/ oil tokens

--- arabia_game/game.py
+++ arabia_game/game.py
@@ -1,6 +1,6 @@
 import pygame
 
-from models import Token, Player
+from models import Token, Player, saudi_arabia
 from random import randint
 from utils import load_surface
 
@@ -15,11 +15,11 @@ class Arabia:
         # Player
         self.player = Player(500, 0)
         # Surfaces
-        self.controls_bg = load_surface("paper.png", False)
+        self.menu_bg = load_surface("paper.png", False)
         self.map = load_surface("map_relief.png", False)
         # Sprites
         self.oil_tokens = [
-            Token("Oil", load_surface("oil_symbol_small.png")) for i in range(3)
+            # Token("Oil", load_surface("oil_symbol_small.png")) for i in range(3)
         ]
         self.font = pygame.font.SysFont("monospace", 35)
 
@@ -42,23 +42,46 @@ class Arabia:
                     quit()
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
+                print(pos)
 
                 for s in self.oil_tokens:
                     if s.rect.collidepoint(pos):
                         # Handle clicking on an oil token
-                        print(f"Clicked on sprite {s.__repr__()}")
                         self.oil_tokens.remove(s)
-                        self.player.oil += 25
+                        if self.sa.collidepoint(pos):
+                            print(f"Clicked on sprite {s.__repr__()} inside of Arabia")
+                            self.player.oil += 25
+                        else:
+                            print(f"Clicked on sprite {s.__repr__()} outside of Arabia")
+                            self.player.oil += 25
+                            self.player.money -= 100
+
 
     def _process_game_logic(self):
-        if randint(1, 20000) == 1:
+        if randint(1, 1_000) == 1:
             self.oil_tokens.append(
                 Token("Oil", load_surface("oil_symbol_small.png"))
             )
+        ...
 
     def _draw(self):
         self.screen.blit(self.map, (MENU_WIDTH, 0))
-        self.screen.blit(self.controls_bg, (0, 0))
+        self.screen.blit(self.menu_bg, (0, 0))
+
+        self.sa = pygame.draw.aalines(
+            self.screen,
+            pygame.Color(125,0,0),
+            True,
+            saudi_arabia,
+        )
+
+        # self.sa = pygame.draw.polygon(
+        #     self.screen,
+        #     pygame.Color(125,0,0),
+        #     saudi_arabia,
+        #     2
+        # )
+
         for token in self.oil_tokens:
             self.screen.blit(
                 token.image, token.rect
